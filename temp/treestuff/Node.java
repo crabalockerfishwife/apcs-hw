16a5acf4d5492a7afc430b4b74a55a574212cd6b
Fish Milnikiewicz <fmilniki@gmail.com>
2014-04-23 10:13:41 -0400

    started tree

---
+++ temp/treestuff/Node.java
@@ -0,0 +1,37 @@
+/*
+copied and pasted linked list node
+ */
+import java.io.*;
+import java.util.*;
+
+public class Node{
+
+    private int data;
+    private Node left,right;
+
+    public Node (int d){
+	int = d;
+	left = null;
+	right = null;
+    }
+
+    public void setData(String d){
+	data = d;
+    }
+
+    public String getData(){
+	return data;
+    }
+
+    public void setNext(Node n){
+	next = n;
+    }
+
+    public Node getNext(){
+	return next;
+    }
+
+    public String toString(){
+	return data;
+    }
+}
