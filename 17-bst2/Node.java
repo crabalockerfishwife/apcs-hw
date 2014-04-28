be39882a67e4dfb877ea3faa4adf48d16329be90
Fish Milnikiewicz <fmilniki@gmail.com>
2014-04-28 19:55:30 -0400

    worked on hw 17 electric boogaloo

---
+++ 17-bst2/Node.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Node{
+
+    private int data;
+    private Node left,right;
+
+    public Node (int d){
+	data = d;
+	left = null;
+	right = null;
+    }
+
+    public void setData(int d){
+	data = d;
+    }
+
+    public int getData(){
+	return data;
+    }
+
+    public void setLeft(Node n){
+	left = n;
+    }
+
+    public void setRight(Node n){
+	right = n;
+    }
+
+    public Node getLeft(){
+	return left;
+    }
+
+    public Node getRight(){
+	return right;
+    }
+
+    public String toString(){
+	return ""+data;
+    }
+}
